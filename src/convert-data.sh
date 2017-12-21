298af2c4b7a0186ad5396f02a609ab9aadc852d7
Aaron Dennis <adennis@azavea.com>
2017-12-21 09:18:08 -0500

    Add naive data conversion script

--- src/convert-data.sh
+++ src/convert-data.sh
@@ -1,3 +1,7 @@
-# Convert data from FileGDB to open formats
-ogr2ogr -t_srs 'EPSG:4326' -f 'GPKG' /app/data/skylands_habitat.gpkg /app/data/skylands_v3_3gdb/skylands_v3_3.gdb Envr_hab_ls_v3_3_skylands
-ogr2ogr -f 'CSV' ./data/skylands_species.csv /app/data/skylands_v3_3gdb/skylands_v3_3.gdb Envr_hab_ls_v3_3_skyland_sp_05
+
+for region in $(cat /usr/local/src/nj-habitat-regions.txt)
+do
+  # Convert data from FileGDB to open formats
+  ogr2ogr -overwrite -t_srs 'EPSG:4326' -f 'GPKG' /app/data/${region}_habitat.gpkg /app/data/landscape-project-3-3/${region}.gdb Envr_hab_ls_v3_3_${region}
+  ogr2ogr -overwrite -f 'CSV' ./data/${region}_species.csv /app/data/landscape-project-3-3/${region}.gdb Envr_hab_ls_v3_3_${region}_sp_05
+done
