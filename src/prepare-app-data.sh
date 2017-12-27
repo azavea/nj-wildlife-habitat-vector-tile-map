30efc106455042fadc2694e6913140812079124b
Aaron Dennis <adennis@azavea.com>
2017-12-27 13:38:45 -0500

    Use centroid for overview tiles and fix prepare app data commands

--- src/prepare-app-data.sh
+++ src/prepare-app-data.sh
@@ -8,12 +8,12 @@ mkdir -p /app/habitat
 
 # Habitat Polygon Vector Tiles
 # Read habitat features and convert to vector tiles
-tippecanoe --read-parallel -z 13 -Z 11 -f -o /app/tiles/habitat-areas.mbtiles ./data/features.txt
+tippecanoe --read-parallel -z 14 -Z 12 -f -o /app/tiles/habitat-areas.mbtiles /app/data/features.txt
 
 # Individual Habitat GeoJSON Files
 # Save habitat features to individual GeoJSON files named by LINKID
-parallel --pipepart -a ./data/features.txt --block 3M node /usr/local/src/create-habitat-individual-json-files.js
+parallel --pipepart -a /app/data/features.txt --block 3M node /usr/local/src/create-habitat-individual-json-files.js
 
 # Habitat Point Overview Vector Tiles
 # Read habitat features line-by-line in parallel, generate species report points, then convert to vector tiles
-parallel --pipepart -a ./data/features.txt --block 3M node /usr/local/src/create-species-sighting-points.js | tippecanoe -rg -f -o /app/tiles/sightings.mbtiles
+parallel --pipepart -a /app/data/features.txt --block 3M node /usr/local/src/create-overview-points.js | tippecanoe -rg -Z 6 -z 13 -ad -f -o /app/tiles/sightings.mbtiles
