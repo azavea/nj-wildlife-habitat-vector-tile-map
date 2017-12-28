e105f4955acc320d69de83e26a425ad8c3cdd57f
Aaron Dennis <adennis@azavea.com>
2017-12-28 10:05:39 -0500

    Simplify parallel command

--- src/prepare-app-data.sh
+++ src/prepare-app-data.sh
@@ -12,8 +12,9 @@ tippecanoe --read-parallel -z 14 -Z 12 -f -o /app/tiles/habitat-areas.mbtiles /a
 
 # Habitat Point Overview Vector Tiles
 # Read habitat features line-by-line in parallel, generate species report points, then convert to vector tiles
-parallel --pipepart -a /app/data/features.txt --block 3M node /usr/local/src/create-overview-points.js | tippecanoe -rg -Z 6 -z 13 -ad -f -o /app/tiles/sightings.mbtiles
+parallel --pipepart -a /app/data/features.txt node /usr/local/src/create-overview-points.js | tippecanoe -rg -Z 6 -z 13 -ad -f -o /app/tiles/sightings.mbtiles
 
 # Individual Habitat GeoJSON Files
 # Save habitat features to individual GeoJSON files named by LINKID
-parallel --pipepart -a /app/data/features.txt --block 3M node /usr/local/src/create-habitat-individual-json-files.js
+echo 'Writing individual GeoJSON files to /app/habitat/...'
+parallel --pipepart -a /app/data/features.txt node /usr/local/src/create-habitat-individual-json-files.js
