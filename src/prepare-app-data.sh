a6dfa37a11157ce05a98f1193d4a425b9829f952
Aaron Dennis <adennis@azavea.com>
2018-01-03 13:10:50 -0500

    Use Tippecanoe to give layer names to tile sources

--- src/prepare-app-data.sh
+++ src/prepare-app-data.sh
@@ -6,13 +6,22 @@ mkdir -p /app/tiles
 #Directory for individual habitat files
 mkdir -p /app/habitat
 
+# Tippecanoe options https://github.com/mapbox/tippecanoe#options
+
 # Habitat Polygon Vector Tiles
 # Read habitat features and convert to vector tiles
-tippecanoe --read-parallel -z 14 -Z 12 -f -o /app/tiles/habitat-areas-2017.mbtiles /app/data/features.txt
+tippecanoe --read-parallel \
+  -n "NJ Landscape Project Habitat Areas" -A "<a href='http://www.state.nj.us/dep/fgw/ensp/landscape/index.htm'>NJDEP</a>" \
+  -l "nj-habitat-areas" -z 14 -Z 12 \
+  -f -o /app/tiles/nj-habitat-areas.mbtiles \
+  /app/data/features.txt
 
 # Habitat Point Overview Vector Tiles
 # Read habitat features line-by-line in parallel, generate species report points, then convert to vector tiles
-parallel --pipepart -a /app/data/features.txt node /usr/local/src/create-overview-points.js | tippecanoe -rg -Z 6 -z 13 -ad -f -o /app/tiles/habitat-overview-2017.mbtiles
+parallel --pipepart -a /app/data/features.txt node /usr/local/src/create-overview-points.js | tippecanoe \
+  -n "NJ Landscape Project Habitat Overview Points" -A "<a href='http://www.state.nj.us/dep/fgw/ensp/landscape/index.htm'>NJDEP</a>" \
+  -l "nj-habitat-overview" -rg -Z 6 -z 13 -ad \
+  -f -o /app/tiles/nj-habitat-overview.mbtiles
 
 # Individual Habitat GeoJSON Files
 # Save habitat features to individual GeoJSON files named by LINKID
