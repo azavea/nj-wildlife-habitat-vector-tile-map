d3f7a13e4fe09e3189d60b9bb714bf5d08638950
lossyrob <rdemanuele@gmail.com>
2018-05-02 21:10:31 -0400

    Export pbf tiles from mb-tiles

--- src/prepare-app-data.sh
+++ src/prepare-app-data.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Directory for tiles
 mkdir -p /app/tiles
@@ -18,12 +18,22 @@ tippecanoe --read-parallel \
 
 # Habitat Point Overview Vector Tiles
 # Read habitat features line-by-line in parallel, generate species report points, then convert to vector tiles
-parallel --pipepart -a /app/data/features.txt node /usr/local/src/create-overview-points.js | tippecanoe \
-  -n "NJ Landscape Project Habitat Overview Points" -A "<a href='http://www.state.nj.us/dep/fgw/ensp/landscape/index.htm'>NJDEP</a>" \
-  -l "nj-habitat-overview" -rg -Z 6 -z 13 -ad \
-  -f -o /app/tiles/nj-habitat-overview.mbtiles
+# parallel --pipepart -a /app/data/features.txt node /usr/local/src/create-overview-points.js | tippecanoe \
+#   -n "NJ Landscape Project Habitat Overview Points" -A "<a href='http://www.state.nj.us/dep/fgw/ensp/landscape/index.htm'>NJDEP</a>" \
+#   -l "nj-habitat-overview" -rg -Z 6 -z 13 -ad \
+#   -f -o /app/tiles/nj-habitat-overview.mbtiles
 
 # Individual Habitat GeoJSON Files
 # Save habitat features to individual GeoJSON files named by LINKID
 echo 'Writing individual GeoJSON files to /app/habitat/...'
 parallel --pipepart -a /app/data/features.txt node /usr/local/src/create-habitat-individual-json-files.js
+
+# Export the mbtiles to pbf files
+function export-mbtiles() {
+    mb-util --image_format=pbf $1 $2;
+    pushd $2 && \
+        gzip -d -r -S .pbf * && \
+        popd;
+}
+
+export-mbtiles /app/tiles/nj-habitat-areas.mbtiles /app/tiles/nj-habitat-areas
