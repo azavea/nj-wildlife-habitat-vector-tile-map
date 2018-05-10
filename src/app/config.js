82f13dc44f483f5d35f43f83a2d6d3f0ace674ac
danford1 <dford@azavea.com>
2018-05-10 13:09:15 -0400

    Add new Mapbox token

--- src/app/config.js
+++ src/app/config.js
@@ -1,23 +1,12 @@
 // Read-scope Mapbox Access Token
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZGFuamZvcmQiLCJhIjoiY2pkZ2VieXQzMGtnODJxcGc0ZWN2NGhmciJ9.wl9Q42ad-LclzE9mkoc16A';
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoibmp3aWxkbGlmZS1hemF2ZWEiLCJhIjoiY2pnemY2dXQ1MTBocDJ3bGxmdmU5YThzcCJ9.hmeBcIvcdhkCHc3LFZ2zlw';
 
-// Protocol, hostname, and port for application (e.g. http://localhost:4000/app)
-// {HOST}/habitat/ This where the app will look for the individual GeoJSON files.
+// This where the app will look for the individual GeoJSON files.
 const HOST = 'https://s3.amazonaws.com/nj-wildlife-habitat-vector-tile-map';
 
 // URLs to the tile source TileJSON resources. Supported protocols are http:, https:, and mapbox://<mapid>.
-// These should be Mapbox Tilesets created by uploading the MBTiles files in app/tiles/
-
-// const TILE_SOURCES = {
-//   // habitatAreas: 'mapbox://danjford.ccm620fv',
-//   habitatAreas: 'http://localhost:8000/tiles/nj-habitat-areas-2/{z}/{x}/{y}.pbf',
-//   habitatOverview: 'mapbox://danjford.5kxue5mn'
-// };
-
 const TILE_SOURCES = {
     habitatAreas: HOST + '/tiles/nj-habitat-areas/{z}/{x}/{y}',
-    // habitatAreas: "http://njwildlife.azavea.com/tiles/nj-habitat-areas/{z}/{x}/{y}"
     // habitatRegions: 'http://localhost:8000/tiles/nj-habitat-overview/{z}/{x}/{y}.pbf.pbf'
-    // habitatAreas: 'mapbox://danjford.ccm620fv',
     habitatRegions: 'mapbox://danjford.89yecrvp'
 };
